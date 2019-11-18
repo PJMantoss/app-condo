9f7e8345843c5a102663c7e4da88aa2da9b82e68
PJMantoss <joelptoss@gmail.com>
2019-11-18 17:54:56 +0100

    Update application.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,3 +15,10 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+import ReactOnRails from 'react-on-rails';
+import Search from '../components/Search';
+
+ReactOnRails.register({
+  Search
+});
