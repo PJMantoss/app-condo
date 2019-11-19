5d1a432ad447b19dd4769893a7e5074f76a942e8
PJMantoss <joelptoss@gmail.com>
2019-11-19 17:32:13 +0100

    add bin/yarn install in react_on_rails initializer

--- config/initializers/react_on_rails.rb
+++ config/initializers/react_on_rails.rb
@@ -1,5 +1,5 @@
 ReactOnRails.configure do |config|
-    config.build_production_command = "RAILS_ENV=production NODE_ENV=production bin/webpack"
+    config.build_production_command = "RAILS_ENV=production NODE_ENV=production bin/yarn install && bin/webpack"
     config.build_test_command = "RAILS_ENV=test bin/webpack"
     config.server_bundle_js_file = ""
 end
